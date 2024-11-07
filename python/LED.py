b3318a61f0675fa1bcfb47e9f86652593961fec2
A112223004 <a112223004@mail.shu.edu.tw>
2024-11-07 15:45:28 +0800

    113/11/07

--- python/LED.py
+++ python/LED.py
@@ -1,6 +1,16 @@
-import machine
+import machine 
 import time
-led_onboard = machine.Pin(15, machine.Pin.OUT)
-while True:
- led_onboard.toggle()
- time.sleep(0.5)
+pin = 'LED'
+led_Builtin = machine.Pin(pin, machine.Pin.OUT)
+led1 = machine.Pin(16, machine.Pin.OUT)
+led2 = machine.Pin(17, machine.Pin.OUT)
+while True: 
+    led_Builtin.value(1) 
+    led1.value(0)
+    led2.value(1)
+    time.sleep(0.1)
+    
+    led_Builtin.value(0) 
+    led1.value(1)
+    led2.value(0)
+    time.sleep(0.1)
